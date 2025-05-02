3394e99ea461d2b657899e2f7d3db6d06f4cbb87
Jimmykotter <jimmykotter@gmail.com>
2025-05-01 19:55:07 -0500

    updated connection.ts

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI || '';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/GridLock';
 
 const db = async (): Promise<typeof mongoose.connection> => {
   try {
@@ -17,3 +17,26 @@ const db = async (): Promise<typeof mongoose.connection> => {
 };
 
 export default db;
+
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techquiz');
+
+// export default mongoose.connection;
+
+// // mongodb+srv://jimmykotter:n3uLqumcvvaorrjh@cluster0.agmqava.mongodb.net/Automerge?retryWrites=true&w=majority&appName=Cluster0
+
+// 'mongodb+srv://gameAdmin:rootroot@gridlock.iajzdqi.mongodb.net/'
+
+
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// import mongoose from 'mongoose';
+
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techquiz');
+
+// export default mongoose.connection;
+
+// // mongodb+srv://jimmykotter:n3uLqumcvvaorrjh@cluster0.agmqava.mongodb.net/Automerge?retryWrites=true&w=majority&appName=Cluster0
+
+// mongodb+srv://gameAdmin:rootroot@gridlock.iajzdqi.mongodb.net/mongodb+srv://gameAdmin:rootroot@gridlock.iajzdqi.mongodb.net/GridLock
+
