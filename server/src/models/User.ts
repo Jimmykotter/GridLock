b303eb1458b3ef382a870f09f4ec32632d1f5d25
Ajthompson88 <aj.thompson8888@gmail.com>
2025-05-05 18:43:52 -0400

    updates

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -2,8 +2,8 @@ import { Schema, model, type Document } from 'mongoose';
 import bcrypt from 'bcrypt';
 
 // import schema from Book.js
-import bookSchema from './Book.js';
-import type { BookDocument } from './Book.js';
+import bookSchema from './Game.js';
+import type { BookDocument } from './Game.js';
 
 export interface UserDocument extends Document {
   id: string;
