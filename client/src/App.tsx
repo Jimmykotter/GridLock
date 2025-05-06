2cd202b481cf1d949af91692abc2c62aec32fd28
leviking12 <leviking.lk@gmail.com>
2025-05-06 00:18:24 -0400

    adjusting logo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,10 @@
-// import React from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { useTransition, animated } from '@react-spring/web';
-
-import "./styles/global.css"
-import LoginPage  from './pages/Login';
+import LoginPage from './pages/Login';
 import SignupPage from './pages/Signup';
-import GameBoard  from './pages/gameboard';
-import ErrorPage  from './pages/errorPage';
+import GameBoard from './pages/gameboard';
+import ErrorPage from './pages/errorPage';
+import './styles/global.css';
 
 export default function App() {
   const location = useLocation();
@@ -15,36 +13,33 @@ export default function App() {
     from:   { opacity: 0, transform: 'translateX(100%)' },
     enter:  { opacity: 1, transform: 'translateX(0%)'    },
     leave:  { opacity: 0, transform: 'translateX(-100%)' },
-    config: { duration: 500 }
+    config: { duration: 500 },
   });
 
   return (
-    <div style={{
-      position: 'relative',
-      width:    '100%',
-      height:   '100vh',
-      overflow: 'hidden'
-    }}>
-      {transitions((style, loc) => (
-        <animated.div
-          style={{
-            ...style,
-            position: 'absolute',
-            top:      0,
-            left:     0,
-            width:    '100%',
-            height:   '100%'
-          }}
-        >
-          <Routes location={loc} key={loc.pathname}>
-            <Route path="/"         element={<LoginPage />}   />
-            <Route path="/login"    element={<LoginPage />}   />
-            <Route path="/signup"   element={<SignupPage />}  />
-            <Route path="/gameboard" element={<GameBoard />}   />
-            <Route path="*"         element={<ErrorPage />}   />
-          </Routes>
-        </animated.div>
-      ))}
+    <div className="app-container">
+      <div className="route-container">
+        {transitions((style, loc) => (
+          <animated.div
+            style={{
+              ...style,
+              position: 'absolute',
+              top:      0,
+              left:     0,
+              width:    '100%',
+              height:   '100%',
+            }}
+          >
+            <Routes location={loc} key={loc.pathname}>
+              <Route path="/"          element={<LoginPage />}   />
+              <Route path="/login"     element={<LoginPage />}   />
+              <Route path="/signup"    element={<SignupPage />}  />
+              <Route path="/gameboard" element={<GameBoard />}   />
+              <Route path="*"          element={<ErrorPage />}   />
+            </Routes>
+          </animated.div>
+        ))}
+      </div>
     </div>
   );
 }
